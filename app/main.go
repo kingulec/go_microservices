ac730a41b087d231ae76b3e68190aeb4e52406d4
kingulec <kinga_kociszewska@interia.pl>
2026-01-12 13:55:01 +0100

    Use env variables

--- app/main.go
+++ app/main.go
@@ -2,58 +2,35 @@ package main
 
 import (
 	"app/app/wbhandler"
-	"fmt"
 	"log"
 	"net/http"
 	"os"
-	"gopkg.in/yaml.v2"
-	"strconv"
 )
 
-
-type Config struct {
-	Service struct {
-		Host string `yaml:"host"`
-		Port int    `yaml:"port"`
-		TLS  struct {
-			CertFile string `yaml:"cert_file"`
-			KeyFile  string `yaml:"key_file"`
-		} `yaml:"tls"`
-	} `yaml:"service"`
-    
-}
-
-func LoadConfig(file string) (*Config, error) {
-	// Read the YAML configuration file and get config
-	data, err := os.ReadFile(file)
-	if err != nil {
-		return nil, err
-	}
-	var cfg Config
-	err = yaml.Unmarshal(data, &cfg)
-	if err != nil {
-		return nil, err
-	}
-	return &cfg, nil
-}
-
 func main() {
-	configFile := "app/config.yaml"
-	conf , err := LoadConfig(configFile)
-	if err != nil {
-		log.Fatal("Failed to load config:", err)
+
+	certPath := os.Getenv("TLS_CERT_PATH")
+	keyPath := os.Getenv("TLS_KEY_PATH")
+	if certPath == "" {
+		certPath = "./certs/tls.crt"
 	}
-	port :=  strconv.Itoa(conf.Service.Port)
-	
+	if keyPath == "" {
+		keyPath = "./certs/tls.key"
+	}
+
+	host := os.Getenv("SERVICE_HOST")
+	if host == "" {
+		host = "0.0.0.0"
+	}
+	port := "8443"
 	if len(os.Args) > 1 && os.Args[1] != "" {
 		port = os.Args[1]
 	}
-	fmt.Println(conf.Service.Host+":"+port)
+	
 	http.HandleFunc("/webhook", wbhandler.WebHookHandler)
 	log.Println("Starting HTTPS server on port " + port)
 	//ListenAndServe starts an HTTPS server with a given address and handler.
-	err2 := http.ListenAndServeTLS(conf.Service.Host+":"+port,
-	 conf.Service.TLS.CertFile,conf.Service.TLS.KeyFile, nil)
+	err2 := http.ListenAndServeTLS(host+":"+port, certPath, keyPath, nil)
 	if err2 != nil {
 		log.Fatal("Failed to start server:", err2)
 	}
